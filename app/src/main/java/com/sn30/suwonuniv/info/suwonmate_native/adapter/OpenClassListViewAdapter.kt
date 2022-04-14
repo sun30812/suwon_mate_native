a5a041a45349b827d482d755a390cbd19c3af04e
sun30812 <sun30812@naver.com>
2022-04-14 16:29:29 +0900

    [기능 추가] 개설 강좌 조회 일부 가능
    학부별 과목 조회 가능

---
+++ app/src/main/java/com/sn30/suwonuniv/info/suwonmate_native/adapter/OpenClassListViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.sn30.suwonuniv.info.suwonmate_native.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.sn30.suwonuniv.info.suwonmate_native.R
+import com.sn30.suwonuniv.info.suwonmate_native.models.InfoCardItem
+import com.sn30.suwonuniv.info.suwonmate_native.models.SimpleCardItem
+import kotlin.coroutines.coroutineContext
+
+class OpenClassListViewAdapter(private val dataList: List<SimpleCardItem>): RecyclerView.Adapter<OpenClassListViewAdapter.ViewHolder>() {
+    private var mListener: OnItemClickListener? = null
+    interface OnItemClickListener {
+        fun onItemClicked(v: View, siteCode: String)
+    }
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.mListener = listener
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.title)
+        val content: TextView = itemView.findViewById(R.id.content)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.simple_list_layout, parent, false)
+        view.isClickable = true
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = dataList[position].title
+        holder.content.text = dataList[position].content
+    }
+
+    override fun getItemCount(): Int = dataList.size
+}
--- app/src/main/java/com/sn30/suwonuniv/info/suwonmate_native/view/OpenClassActivity.kt
+++ app/src/main/java/com/sn30/suwonuniv/info/suwonmate_native/view/OpenClassActivity.kt
@@ -2,49 +2,100 @@ package com.sn30.suwonuniv.info.suwonmate_native.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.sn30.suwonuniv.info.suwonmate_native.R
+import com.sn30.suwonuniv.info.suwonmate_native.adapter.OpenClassListViewAdapter
 import com.sn30.suwonuniv.info.suwonmate_native.databinding.OpenClassBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONArray
-import org.json.JSONObject
-import org.json.JSONTokener
+import com.sn30.suwonuniv.info.suwonmate_native.models.CardItem
+import com.sn30.suwonuniv.info.suwonmate_native.models.InfoCardItem
+import com.sn30.suwonuniv.info.suwonmate_native.models.SimpleCardItem
 
 class OpenClassActivity : AppCompatActivity() {
     private val TAG = "FirebaseTest"
     private lateinit var binding: OpenClassBinding
+    private lateinit var majorDataSnapshot: DataSnapshot
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = OpenClassBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        CoroutineScope(Dispatchers.Main).launch {
-            getDB()
-        }
-    }
-    private suspend fun getDB(): Boolean = withContext(Dispatchers.IO) {
+        val departmentsListAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line)
+        val majorListAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line)
+        val gradeListAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,resources.getStringArray(
+            R.array.grades))
+        binding.departments.adapter = departmentsListAdapter
+        binding.majors.adapter = majorListAdapter
+        binding.grades.adapter = gradeListAdapter
+        var department = ""
+        var major = ""
+        val subjectList: ArrayList<SimpleCardItem> = arrayListOf()
+        val adapter = OpenClassListViewAdapter(subjectList)
+        binding.classListView.adapter = adapter
+        binding.classListView.layoutManager = LinearLayoutManager(this)
         val database = Firebase.database
-        val myRef = database.getReference("estbLectDtaiList_test/0/컴퓨터학부")
+        val myRef = database.getReference("/departments")
+        val mySubjectRef = database.getReference("/estbLectDtaiList")
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.value
-//                val subjectList = JSONObject(value.toString())
-                Log.d(TAG, "value is ${value}")
+                majorDataSnapshot = snapshot
+                majorDataSnapshot.children.forEach {
+                    departmentsListAdapter.add(it.key.toString())
+                }
+                binding.spinnerGroup.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.GONE
+
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w(TAG, "onCancelled: ", error.toException())
             }
         })
-        true
-    }
+        binding.departments.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                p0: AdapterView<*>?,
+                p1: View?,
+                p2: Int,
+                p3: Long
+            ) {
+                department = binding.departments.selectedItem.toString()
+                majorListAdapter.clear()
+                subjectList.clear()
+                majorDataSnapshot.child(binding.departments.selectedItem.toString()).children.forEach { dat ->
+                    majorListAdapter.add(dat.value.toString())
+                }
+                mySubjectRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        snapshot.child(department).children.forEach {
+                            subjectList.add(
+                                SimpleCardItem(
+                                    it.child("subjtNm").value.toString(),
+                                    it.child("ltrPrfsNm").value.toString()
+                                )
+                            )
+                            adapter.notifyDataSetChanged()
+                        }
+                    }
 
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.w(TAG, "Can't access because: $error")
+                    }
+                })
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
+    }
 }
