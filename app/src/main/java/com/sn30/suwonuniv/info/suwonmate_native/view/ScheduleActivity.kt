c9f09a0f5912e0b1893a5fd2d5f6350b25954e99
sun30812 <sun30812@naver.com>
2022-07-26 15:46:14 +0900

    학사 일정 메뉴에 현재 일정이 보이도록 개선

--- app/src/main/java/com/sn30/suwonuniv/info/suwonmate_native/view/ScheduleActivity.kt
+++ app/src/main/java/com/sn30/suwonuniv/info/suwonmate_native/view/ScheduleActivity.kt
@@ -1,11 +1,14 @@
 package com.sn30.suwonuniv.info.suwonmate_native.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.sn30.suwonuniv.info.suwonmate_native.R
 import com.sn30.suwonuniv.info.suwonmate_native.databinding.ScheduleBinding
 import com.sn30.suwonuniv.info.suwonmate_native.adapter.ScheduleViewAdapter
 import com.sn30.suwonuniv.info.suwonmate_native.models.SimpleCardItem
@@ -15,6 +18,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class ScheduleActivity: AppCompatActivity() {
     private lateinit var binding: ScheduleBinding
@@ -46,15 +51,31 @@ class ScheduleActivity: AppCompatActivity() {
             }
             true
         }
+        fun getTodaySchedule(): String{
+            val now = LocalDate.now()
+            for (date in list) {
+                val data = date.content.replace(Regex("\\([^)]*\\)"), "")
+                    .replace(".","-")
+                if (now == LocalDate.parse(data.substring(0..9))) {
+                    return date.title
+                } else if (data.contains('~')) {
+                    if ((now >= LocalDate.parse(data.substring(0..9))) &&
+                            (now <= LocalDate.parse(data.substring(13..22)))) {
+                        return date.title
+                    }
+                }
+            }
+            // 오늘날자와 대조하는 날짜를 찾는 알고리즘 가져오기
+            return getString(R.string.none)
+        }
         CoroutineScope(Dispatchers.Main).launch {
              getData()
             binding.loading.visibility = ProgressBar.GONE
             binding.scheduleRootView.gravity = Gravity.TOP
             binding.scheduleMainView.visibility = RecyclerView.VISIBLE
+            binding.today.text = getString(R.string.today_schedule, getTodaySchedule())
+            binding.today.visibility = TextView.VISIBLE
             adapter.notifyItemInserted(0)
         }
-
-
-
     }
 }
